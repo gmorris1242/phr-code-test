f74bb8a210d964fb1c2b1ed6bc67306474178aa0
George Morris <georgemorris@Georges-MacBook-Pro-2.local>
2025-02-10 14:19:17 -0500

    heroku testing

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,6 @@
 import { Application } from '@hotwired/stimulus'
-import TicketController from "./ticket_controller";
 
 const application = Application.start()
-
-application.register("ticket", TicketController);
 
 // Configure Stimulus development experience
 application.debug = false
